5ada0a32a2ff0108d5a3ec76baaa9b38f9da108e
HnpawsPham <hanpham123th@gmail.com>
2026-02-27 00:13:11 +0700

    implement basic model interactions and fix some bugs

--- public/scripts/game_theory/manager.js
+++ public/scripts/game_theory/manager.js
@@ -1,18 +1,37 @@
 import {Model} from "./models.js";
-import {addScoreTag, loadModelPreview} from "./UI.js";
 
 let modelList = Model.children.map(elm => new elm());
 
-for(let model of modelList)
-    for(let opponent of modelList){
-        let opponentMove = opponent.strategy(model.firstTry);
-        
-        if(opponentMove < model.firstTry)
-            model.point--;
-        else model.point++;
-    }
+export function getModelList() {
+    return modelList;
+}
 
-for(let model of modelList){
-    addScoreTag(model);
-    loadModelPreview(model);
+function sleep(ms) {
+    return new Promise(r => setTimeout(r, ms));
+}
+
+let gameSpeed = 300;
+export function changeGameSpeed(value){
+    gameSpeed = value;
+    console.log(gameSpeed);
+}
+
+export async function runGame(models, onMatch) {
+    for (let model of models) {
+        for (let opponent of models) {
+            if (model.name == opponent.name) continue;
+
+            let opponentMove = opponent.strategy(model.return);
+
+            if(opponentMove < model.return)
+                model.point -= 3;
+            else if(opponentMove > model.return)
+                model.point += 3;
+            else if(opponentMove + model.return > 0) 
+                model.point++;
+
+            onMatch(model, opponent);
+            await sleep(gameSpeed);
+        }
+    }
 }
